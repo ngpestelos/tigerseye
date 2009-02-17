53480d0e437e9909ce89862f1c4199d10d205409
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-02-18 00:33:45 +0800

    bulk loading of feed entries

--- tigerseye/entries.py
+++ tigerseye/entries.py
@@ -1,34 +1,42 @@
+# -*- coding: utf-8 -*-
+
 """ A module for capturing feed entries. 
 
     Creates a Document for each feed URL, with its list of entries.
 """
 from glob import glob
 import feedparser
+from couchdb import Server
+import hashlib
 
 def mark_as_failed(url):
     "Create a record indicating a problem on feed capture"
     pass
 
-def load_from_dir(feed_dir):
+def load_from_dir(feed_dir, dbname='feeds'):
     "Import from a group of feed dumps."
-    def create_document(link, title, entries):
-        print "creating doc"
+
+    def create_document(db, link, title, entries):
+        print "Creating document for %s" % link
+        stripped = []
+        for e in entries:
+            try:
+                e1 = {'link': e['link'], \
+                  'title': e['title'], 'description': \
+                  e.get('description', '') or e.get('subtitle', '')}
+                stripped.append(e1)
+            except:
+                print "Problem with entry %s" % e['link']
+        
+        db.create({'type': 'entries', 'link': link, 'title': title, \
+          'entries': stripped})
 
     for f in glob("%s/*.txt" % feed_dir):
         d = feedparser.parse(f)
-        link = ''
-        title = ''
+        db = Server()[dbname]
         try:
-            link = d.feed.link
-            title = d.feed.title
-            entries = d.get('entries', [])
-            if link:
-                create_document(link, title, entries)
-            else:
-                print "Skipping %s" % f
+            create_document(db, d.feed.link, d.feed.title, d.entries)
         except:
-            print "Unable to parse correctly: %s" % f
-
-        #print d.entries[0]
+            print "Problem with feed %s" % f
 
 load_from_dir('/n/data/feeds')
