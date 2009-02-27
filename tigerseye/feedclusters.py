adf1b4536ee0fa1120760ed9f1283bf81c549fab
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-02-27 10:00:27 +0800

    hierarchical clustering (from PCI)

--- tigerseye/feedclusters.py
+++ tigerseye/feedclusters.py
@@ -84,3 +84,79 @@ def pearson(v1, v2):
     if den == 0: return 0
 
     return 1.0 - num / den
+
+def get_rows(rownames, data):
+    urls = []
+    rows = []
+
+    for id, url in rownames.items():
+        urls.append(url)
+        rows.append(data[id])
+
+    return urls, rows
+
+class bicluster:
+    def __init__(self, vec, left=None, right=None, distance=0.0, id=None):
+        self.left = left
+        self.right = right
+        self.vec = vec
+        self.id = id
+        self.distance = distance
+
+def hcluster(rows, distance=pearson):
+    distances = {}
+    currentclustid = -1
+
+    # Clusters are initially just the rows
+    clust = [bicluster(rows[i], id=i) for i in range(len(rows))]
+
+    while len(clust) > 1:
+        lowestpair = (0, 1)
+        closest = distance(clust[0].vec, clust[1].vec)
+
+        # Loop through every pair looking for the smallest distance
+        for i in range(len(clust)):
+            for j in range(i+1, len(clust)):
+                # distances is the cache of distance calculations
+                if (clust[i].id, clust[j].id) not in distances:
+                    distances[(clust[i].id, clust[j].id)] = \
+                        distance(clust[i].vec, clust[j].vec)
+
+                d = distances[(clust[i].id, clust[j].id)]
+
+                if d < closest:
+                    closest = d
+                    lowestpair = (i, j)
+
+        # calculate the average of the two clusters
+        mergevec = [ \
+          (clust[lowestpair[0]].vec[i] + clust[lowestpair[1]].vec[i]) / 2.0 \
+            for i in range(len(clust[0].vec))] 
+
+        # create the new cluster
+        newcluster = bicluster(mergevec, left=clust[lowestpair[0]], \
+          right=clust[lowestpair[1]], distance = closest, id = currentclustid)
+
+        # cluster IDs that weren't in the original set are negative
+        currentclustid =- 1
+        del clust[lowestpair[1]]
+        del clust[lowestpair[0]]
+        clust.append(newcluster)
+
+    return clust[0]
+
+
+def printcluster(clust, labels=None, n=0):
+    # indent to make a hierarchy layout
+    for i in range(n): print ' ',
+    if clust.id < 0:
+        # negative id means that this is a branch
+        print '-'
+    else:
+        # positive id means that this is an endpoint
+        if labels == None: print clust.id
+        else: print labels[clust.id]
+
+    # now print the left and right clusters
+    if clust.left != None: printcluster(clust.left, labels, n = n+1)
+    if clust.right != None: printcluster(clust.right, labels, n = n+1)        
