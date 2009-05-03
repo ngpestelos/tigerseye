39294dceb564b227581091e0951a85009ce8a0ac
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-05-03 13:59:02 +0800

    remove tuples
    print which URL is being processed

--- tigerseye/feeds.py
+++ tigerseye/feeds.py
@@ -21,22 +21,27 @@ def load_from_dir(feed_dir, dbname):
     for f in glob("%s/*.txt" % feed_dir):
         load_from_file(f, db)
 
-def insert_feed(feed, entries, db):
+def remove_tuples(feed, entries):
     if feed.has_key('updated_parsed'):
         del feed['updated_parsed']
-    feed['type'] = 'feed'
     for entry in entries:
         if entry.has_key('updated_parsed'):
             del entry['updated_parsed']
         if entry.has_key('published_parsed'):
             del entry['published_parsed']
+    return feed, entries
+
+def insert_feed(feed, entries, db):
+    feed['type'] = 'feed'
+    feed, entries = remove_tuples(feed, entries)
     feed['entries'] = entries
     try:
         db.create(feed)
     except:
-        print "Could not load", feed.link
+        print "Could not load", feed.get('link', feed.get('title', ''))
 
 def load_from_file(filename, db):
     "Extract entries from a file"
     d = feedparser.parse(file(filename))
+    print "Processing", d.feed.get('link', d.feed.get('title', ''))
     insert_feed(d.feed, d.entries, db)
